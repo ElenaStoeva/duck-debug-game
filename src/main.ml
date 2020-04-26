b3171b570e9438ea08d04a372be968bc87291e8f
Yagmur Dulger <yd333@cornell.edu>
2020-04-26 18:33:58 +0300

    Fixed instructions in main

--- src/main.ml
+++ src/main.ml
@@ -80,7 +80,8 @@ let main () =
     "Welcome.\n\
      Game objective:\nWrite code to move agent and achieve winning state.\n\
      Game language syntax:\n\
-     'M' - Move forward\n'R' - Turn right\n'L' - Turn left\n\
+     'M' - Move Forward\n'R' - Turn Right\n'L' - Turn Left\n\
+     '1' - Color Square Red\n'2' - Color Square Green\n'3' - Color Square Blue\n\
      'f=' - define function f\n'[f]' - call function f\n\
      ';' - seperator between function definitions.\n\
      Please enter a level number (1,2,3) to play, or (q) to quit: \n";
